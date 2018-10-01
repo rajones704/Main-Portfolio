8f3112d7fef89e1bcdf6c40be7e5384a6afd1b80
R Jones <rajones704@gmail.com>
2018-10-01 18:24:52 -0400

    updated

---
+++ links/fflinks/app/data/friends.js
@@ -0,0 +1,164 @@
+var friendsArray = [
+	{
+		name: 'Bart Simpson',
+		photo: 'https://cdn1.thr.com/sites/default/files/imagecache/portrait_300x450/2012/02/bart_simpson.jpg',
+		scores: [
+			'1',
+			'5',
+			'5',
+			'1',
+			'5',
+			'5',
+			'1',
+			'1',
+			'1',
+			'5'
+		]
+	},
+	{
+		name: 'Ellen Degeneres',
+		photo: 'https://cdn1.thr.com/sites/default/files/imagecache/NFE_portrait/2017/03/ellen_headshot_8.26-p_2017.jpg',
+		scores: [
+			'2',
+			'4',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'2',
+			'1',
+			'5'
+		]
+	},
+	{
+		name: 'Tyrion Lannister',
+		photo: 'https://cdn.app.compendium.com/uploads/user/758c2424-6055-4d3e-880c-4021718d814e/2be5833d-4622-4ecf-aae1-58b4c227c2a0/File/8ec92a9179e41bde84c5f839a8fd5061/tyrion.jpg',
+		scores: [
+			'4',
+			'3',
+			'5',
+			'1',
+			'1',
+			'5',
+			'5',
+			'1',
+			'1',
+			'1'
+		]
+	},
+	{
+		name: 'NaS',
+		photo: 'https://images-na.ssl-images-amazon.com/images/I/71FHpC13d6L._SY355_.jpg',
+		scores: [
+			'5',
+			'1',
+			'2',
+			'1',
+			'3',
+			'4',
+			'1',
+			'3',
+			'3',
+			'4'
+		]
+	},
+	{
+		name: 'Thanos',
+		photo: 'https://pbs.twimg.com/media/DWoqlrZWsAEH4-1.jpg',
+		scores: [
+			'1',
+			'1',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'1'
+		]
+	},
+	{
+		name: 'Gloria Steinem',
+		photo: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS75XhwiT-k9-XZXipE6RKkxg-ew7gmlO6sNq-HJzIJTtoZrzzb',
+		scores: [
+			'1',
+			'1',
+			'5',
+			'1',
+			'3',
+			'2',
+			'5',
+			'1',
+			'1',
+			'5'
+		]
+	},
+	{
+		name: 'Beyonce',
+		photo: 'http://www.billboard.com/files/styles/article_main_image/public/media/beyonce-feb-2014-billboard-650.jpg',
+		scores: [
+			'5',
+			'4',
+			'4',
+			'2',
+			'4',
+			'4',
+			'3',
+			'2',
+			'3',
+			'3'
+		]
+	},
+	{
+		name: 'Andy Cohen',
+		photo: 'https://media.npr.org/assets/img/2014/11/21/ap195366502606-6de69eeabd22fb7669c8a218f2f97cb21bdf2e12-s800-c85.jpg',
+		scores: [
+			'5',
+			'2',
+			'3',
+			'1',
+			'5',
+			'1',
+			'3',
+			'1',
+			'1',
+			'5'
+		]
+	},
+	{
+		name: 'Kermit the Frog',
+		photo: 'http://i.dailymail.co.uk/i/pix/2015/09/02/08/2BE5406100000578-3218820-image-m-25_1441179775589.jpg',
+		scores: [
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5'
+		]
+	},
+	{
+		name: 'Dalai Lama',
+		photo: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQOqUtZjbSbmL0q7Rma8SeGxplCadoxXtP6Y_618WmREpQpAIM6',
+		scores: [
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1'
+		]
+	}
+];
+
+module.exports = friendsArray;
